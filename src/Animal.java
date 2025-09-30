ba3abffd25223c135fb93f2141e5233ad75ebfbe
sarah <sarahguesmi223@gmail.com>
2025-09-30 11:30:27 +0100

    prosit4
    # Conflicts:
    #	src/Animal.java
    #	src/tn/esprit/gestionzoo/entities/Zoo.java
    #	src/tn/esprit/gestionzoo/main/zooManagement.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,18 +1,29 @@
 public class Animal {
-    int age;
-    String name;
     String family;
+    String name;
+    int age;
     boolean isMammal;
 
-    Animal(String family, String name, int age, boolean isMammal) {
+    public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
         this.age = age;
         this.isMammal = isMammal;
     }
-    Animal(){
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Animal other = (Animal) obj;
+        return age == other.age &&
+                isMammal == other.isMammal &&
+                name.equals(other.name) &&
+                family.equals(other.family);
     }
-    public void displayAnimal() {
-        System.out.println("Animal: " + name + ", Family: " + family + ", Age: " + age + ", Mammal: " + isMammal);
+
+    @Override
+    public String toString() {
+        return "Animal{name='" + name + "', family='" + family + "', age=" + age + ", isMammal=" + isMammal + "}";
     }
 }
